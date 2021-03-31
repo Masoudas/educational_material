98a4c1e669b60ee05d8a152f2361180732c6115c
Masoudas <Masoudas@hotmail.com>
2021-03-31 15:39:47 +0200

    Linux - More on files

--- OpenCV/BuildingComputerVisionProjectswithOpenCV4andCpp/BasicsOfOpenCV_2/ImageRepresentation_2/MatClass_1/7_2_MatExpr_class.cpp
+++ OpenCV/BuildingComputerVisionProjectswithOpenCV4andCpp/BasicsOfOpenCV_2/ImageRepresentation_2/MatClass_1/7_2_MatExpr_class.cpp
@@ -27,6 +27,9 @@
  * Question: Is MatExpr a view of the original matrix? Yes! Note that through the constructor of MatExpr, we can
  * convert a Mat into a MatExpr. Moreover, MatExpr has an implicit cast to Mat. Once the cast operation takes place,
  * the new matrix is ready for use.
+ * What's implied here is that we can make as many complicated expressions as we desire, and once we convert to Mat,
+ * then that new mat is generated. Note that each cast to Mat generates a new underlying structure, which is quite
+ * interesting!
  * 
  */
 
@@ -48,11 +51,15 @@ void MatExpr_is_a_view(){
     auto result_ptr = ((cv::Mat&)result_matExpr).ptr<uchar>();
     if (m.ptr<uchar>() != result_ptr && m1.ptr<uchar>() != result_ptr)
     {
-        std::cout << "\n\nNote that the pointer is not the same for the three mats\n\n";
+        std::cout << "\n\nNote that the pointer is not the same for the three mats\n";
     }
     
+    cv::Mat result_2 = result_matExpr;  // A new cast to Mat does not generate a new matrix.
+    if (result_2.ptr<uchar>() != result_ptr){
+        std::cout << "Result of new cast produces a new data structure.\n\n";
+    }
 }
-
+Structure of Matexpr. Then some examples on expressions.
 // int main(){
 //     MatExpr_is_a_view();
 // }
