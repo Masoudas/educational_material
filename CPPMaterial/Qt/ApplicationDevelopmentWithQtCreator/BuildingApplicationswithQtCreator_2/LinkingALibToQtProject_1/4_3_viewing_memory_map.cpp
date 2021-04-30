fb89b7b786d404153e59ccc32421a97263902bfc
Masoudas <Masoudas@hotmail.com>
2021-04-30 14:40:15 +0200

    More on Qt menues

---
+++ CPPMaterial/Qt/ApplicationDevelopmentWithQtCreator/BuildingApplicationswithQtCreator_2/LinkingALibToQtProject_1/4_3_viewing_memory_map.cpp
@@ -0,0 +1,14 @@
+/**
+ * In addition to viewing and changing variable values, you can view and change individual memory locations. You 
+ * might want to do this if you're debugging a decoder or encoder for a binary format, for example, where you 
+ * need to see a specific location in the memory. From the variables pane, you have several choices by which you 
+ * can check a memory location:
+ * 
+ *  -   You can right-click on a given variable and open a memory window at this variable's address.
+ *  -   You can right-click on a given variable and open a memory window at the value that the variable points 
+ *      to (in other words, dereference a pointer to a memory location).
+ *  -   You can right-click on the variable pane and open up a memory browser at the beginning of the current 
+ *      stack frame.
+ *  -   You can right-click on the variable pane and open up a memory browser at an arbitrary location in the 
+ *      memory.
+ */
