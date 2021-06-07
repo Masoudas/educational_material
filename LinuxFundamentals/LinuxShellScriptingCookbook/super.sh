95f7ae9806650d27ade58f5038cb660faeb177ca
Masoudas <Masoudas@hotmail.com>
2021-06-07 12:23:50 +0200

    Shell - More on PS vars

--- LinuxFundamentals/LinuxShellScriptingCookbook/super.sh
+++ LinuxFundamentals/LinuxShellScriptingCookbook/super.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #Filename: sleep.sh 
-function DEBUG() 
-{ 
-    [ "$_DEBUG" == "on" ] && $@ || : 
+
+f(){
+    echo "$@"
+    echo "$*"
 }
- 
-for i in {1..10} 
+
+f 1 33 3 'a;' a
+
+PS3="Choose case: "
+select i in case1 case2 case3
 do 
-    DEBUG echo "I is $i" 
+    case $i in
+    case1) echo "Case1";;
+    case2) echo "Case2";;
+    case3) exit;;
+    esac
 done
