d9778283a126971dd4d15fba3262682a21127b93
Masoudas <Masoudas@hotmail.com>
2021-06-11 15:13:57 +0200

    Shell - More on condition and checks

--- LinuxFundamentals/LinuxShellScriptingCookbook/ShellSomethingOut_1/script.sh
+++ LinuxFundamentals/LinuxShellScriptingCookbook/ShellSomethingOut_1/script.sh
@@ -1,5 +1,21 @@
-var="Why does the old woman in front of me want to kill me?"
-for i in `seq 0 ${#var}`
-do
-	echo  ${var:$i:1}
+read ip
+
+oldIFS=$IFS	# IFS affects the echo output, so we should keep the old one.
+IFS='.'
+tokens=($ip)
+IFS=$oldIFS
+
+for word in ${tokens[*]}; do
+	octet=$(bc <<< "obase=2; $word")
+
+	# A very bizzare way of making sure the output has exactly eight digits!
+	octet=$(bc <<< "obase=2; ibase=2; $octet + 100000000")
+	octet=${octet:1:8}	# Alternatively, octet=$(expr substr $octet 2 8)
+
+	# Concatenating ip.
+	binary_ip+="$octet."
 done
+
+binary_ip=${binary_ip:0:35} # Remove final dot.	
+# Alternatively, binary_ip=$(expr substr $binary_ip 1 35)
+echo $binary_ip
