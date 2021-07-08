1b33beadef2bc5e6c0d7f2ec64c419b3ccea560b
Masoudas <Masoudas@hotmail.com>
2021-07-08 17:01:22 +0200

    More on ownership

--- LinuxFundamentals/LinuxSystemProgramming/5_working_with_file_IO/5_1_getting_ownership_and_access.c
+++ LinuxFundamentals/LinuxSystemProgramming/5_working_with_file_IO/5_1_getting_ownership_and_access.c
@@ -5,6 +5,23 @@
  * 
  * The functions that we seek are getpwuid and getgrgid. These functions are in the unistd header as we may recall.
  * The following code example shows how these babies work.
+ * 
+ * getpwuid: get uid password as a passwd structure, where passwd is in the pwd header and contains the following
+ * properties:
+ *      char    *pw_name   User's login name.
+ *      uid_t    pw_uid    Numerical user ID.         
+ *      gid_t    pw_gid    Numerical group ID.           
+ *      char    *pw_dir    Initial working directory.           
+ *      char    *pw_shell  Program to use as shell. 
+ * Consequently, passwd contains user information.
+ * 
+ * getgrgid: Information regarding group as a group structure. This fields associated (according to the grp
+ * header: man grp.h) are:
+ *      char   *gr_name;        group name               
+ *      char   *gr_passwd;      group password
+ *      gid_t   gr_gid;         group ID
+ *      char  **gr_mem;         NULL-terminated array of pointers to names of group members 
+ * 
  */
 
 #include <stdio.h>
@@ -45,11 +62,15 @@ int main(int argc, char *argv[])
         perror("Can't get groupname");
         return errno;
     }
+
     printf("Inode: %lu\n", filestat.st_ino);
     printf("Size: %zd\n", filestat.st_size);
     printf("Links: %lu\n", filestat.st_nlink);
     printf("Owner: %d (%s)\n", filestat.st_uid, userinfo->pw_name);
     printf("Group: %d (%s)\n", filestat.st_gid, groupinfo->gr_name);
     printf("File mode: %o\n", filestat.st_mode);
+    if (S_ISREG(filestat.st_mode)){
+        printf("A regular file\n");
+    }
     return 0;
 }
