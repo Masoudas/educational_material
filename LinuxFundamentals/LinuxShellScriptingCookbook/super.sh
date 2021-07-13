3e879334f1d9370aff3b288ec32a57444c2c9759
Masoudas <Masoudas@hotmail.com>
2021-07-13 12:54:40 +0200

    More on splicing

--- LinuxFundamentals/LinuxShellScriptingCookbook/super.sh
+++ LinuxFundamentals/LinuxShellScriptingCookbook/super.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-#Filename: sleep.sh 
+declare -A arr
+arr["a"]="abadsa"
+arr["Aa"]="cd"
 
+for key in "${!arr[@]}"	# The use of quotes is necessary!
+do
+	echo  Key is $key and value is ${arr[$key]}
+done
+
+for pair in ${arr[@]};
+do
+	echo $pair	# Prints the key value pair as [key]:pair
+done
 
