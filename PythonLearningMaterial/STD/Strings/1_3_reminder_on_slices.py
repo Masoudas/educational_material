d17fec51d687d2f71ca133464e2995866fe0cecd
Masoudas <Masoudas@hotmail.com>
2021-10-04 15:50:06 +0330

    On slicing

--- PythonLearningMaterial/STD/Strings/1_3_reminder_on_slices.py
+++ PythonLearningMaterial/STD/Strings/1_3_reminder_on_slices.py
@@ -3,6 +3,10 @@ Recall that slices in python are exclusive. As such, the last index is never cho
 
 Recall also that to invert a string, we may use negative slicing (and I tend to forget to indicate that the steps
 should be -1, and I always put +1)
+
+Me: Note that assignment to slices is not possible. Why? Because string immutability is violated as such, and
+we need to create a new string in any case!
+Also recall that with negative indexes, the last index is always -1, and the first one is exactly -len(str)
 """
 
 string = "This is my string"
@@ -11,3 +15,8 @@ print(sub_str)
 
 reverse_str = string[-1::-1]	# Or more simply [::-1]
 print(reverse_str)
+
+# Assigning to string slices is impossible! We must create a new string
+# string[:4] = "T", "h", "a", "t"
+string2 = "That" + string[4:]
+print(string2)
