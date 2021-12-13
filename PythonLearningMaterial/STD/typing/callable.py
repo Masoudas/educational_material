acb6decb75b027cbd43b9d57e2d1dddb846c0afa
Masoudas <Masoudas@hotmail.com>
2021-12-13 09:19:58 +0330

    On typing

---
+++ PythonLearningMaterial/STD/typing/callable.py
@@ -0,0 +1,7 @@
+"""
+Used for annotating callable (functions).
+"""
+from typing import Callable, List
+
+def myMethod(method: Callable[int, List[str]]) -> None:
+    return None
---
+++ PythonLearningMaterial/STD/typing/optional.py
@@ -0,0 +1,7 @@
+"""
+For optional arguments, we may use Optional annotation.
+"""
+from typing import Optional
+
+def has_optional(v1 : int, opt: Optional[str] = "This is optional"):
+    return None
