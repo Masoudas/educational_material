3d5c53ce58abcf80829e32f79ea8d313360fa37e
Masoudas <Masoudas@hotmail.com>
2022-01-24 08:58:50 +0330

    More on djange apps

--- PythonLearningMaterial/Django/Tutorial/3_FirstApplication/6_1_writing_the_first_view.py
+++ PythonLearningMaterial/Django/Tutorial/3_FirstApplication/6_1_writing_the_first_view.py
@@ -1,7 +1,27 @@
 """
-Inside the view file, we can simply return an string, as follows:
+Inside the view file, we can simply return an string, as follows.
 
+This is the simplest view possible in Django. To call the view, we need to map it to a URL - and for this we need a URLconf.
 
+Now, CREATE a urls.py inside the polls project, and then add the path to it
+###
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+###
+
+The next step is to point the root URLconf at the polls.urls module. In mysite/urls.py, add an import for django.urls.include and insert an include() in the urlpatterns list, so you have:
+###
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls', include('polls.urls'))
+]
+###
+The include() function allows referencing other URLconfs. Whenever Django encounters include(), it chops off whatever part of the URL matched up to that point and sends the remaining string to the included URLconf for further processing.
 """
 
 from django.http import HttpResponse
---
+++ PythonLearningMaterial/Django/Tutorial/3_FirstApplication/6_2_when_to_use_include.py
@@ -0,0 +1,5 @@
+"""
+When to use include()
+
+You should always use include() when you include other URL patterns. admin.site.urls is the only exception to this.
+"""
