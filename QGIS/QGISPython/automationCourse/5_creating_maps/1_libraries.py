db2a180ddc55607739e9949de64909406954e72c
Masoudas <Masoudas@hotmail.com>
2022-02-13 08:46:20 +0330

    Remove database

---
+++ QGIS/QGISPython/automationCourse/5_creating_maps/1_libraries.py
@@ -0,0 +1,14 @@
+"""
+Leaflet is a popular JavaScript library for creating interactive maps for webpages (OpenLayers is another JavaScript library for the same purpose).
+
+Here, will focus on two Python libraries -mplleaflet and Folium - that are able to convert our data in (geo)pandas into interactive Leaflet maps.
+
+Other interesting libraries for creating interactive visualizations from spatial data:
+    -   mapboxgl
+    -   Bokeh
+    -   Geoviews
+
+We can also convert maptlotlib plots directly to interactive web maps using mllpleaflet. All you need to do is to:
+    -   visualize your data using matplotlib (or geopandas plot())
+    -   convert the plot into a webmap using mplleaflet
+"""
---
+++ QGIS/QGISPython/automationCourse/5_creating_maps/2_from_static_map.py
@@ -0,0 +1,7 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import mplleaflet as mpl
+from pathlib import Path
+
+path = Path("QGISPython", "automationCourse", "L2_data", "Europe_borders.shp")
+
