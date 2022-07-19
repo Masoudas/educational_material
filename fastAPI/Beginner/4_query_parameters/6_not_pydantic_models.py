35a325c460c3ccdc222df5ad06771338a3177172
masoudas <masoudas@hotmail.com>
2022-07-19 09:25:11 +0430

    Update doc

--- fastAPI/Beginner/4_query_parameters/6_not_pydantic_models.py
+++ fastAPI/Beginner/4_query_parameters/6_not_pydantic_models.py
@@ -1,3 +1,5 @@
 """
 Me: This may seem odd to mention here, but query params can't be pydantic models it does appear. Why do I mention this? Becuase we can mix query params with path and request (body) params, and use them all together. If we could use pydantic models as query params, we wouldn't be able to distinguish between query params and request body. 
+
+As such, if we require a pydantic model, it has to be defined in body.
 """
